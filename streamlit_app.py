91e7e565430c34242ccede7207db959a859a9fa0
Vignesh Shankar <120453679+VigneshS8@users.noreply.github.com>
2025-11-26 14:19:35 +0530

    Update streamlit_app.py
    Added dataframe to the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,6 @@ if ingredients_list:
 #API request to pull frutis data
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-    
+# st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
